74547a1b7305e6ea85e3604ff1b24c28933e68b4
vadim <vadim.gra4ev2708@gmail.com>
2024-12-29 14:42:26 +0300

    first try

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
 import asyncio
 
+import logging #для логирования (отладки)
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
+from aiogram.client.default import DefaultBotProperties  #редактирование сообщений HTML
+from aiogram.filters import Command
 
+#хранение токенов
 from dotenv import load_dotenv
 import os
 
+from pyexpat.errors import messages
 
+#Загрузка токена
 load_dotenv()
 token = os.getenv('TOKEN')
+admin_id = os.getenv('ADMIN_ID')
 
-async def start_bot(bot: Bot, m: Message):
-    await bot.send_message(1034762341, f'start bot')
+#Загрузка бота
+async def start_bot(bot: Bot):
+
+    await bot.send_message(admin_id, f'start bot')
+# async def get_start(bot: Bot,m: Message):
+    # await bot.send_message(m.from_user.id,f'{m.from_user.id}')
+
 async def get_start(m: Message, bot: Bot):
-    await bot.send_message(m.from_user.id, f'привет')
-async def start():
-    bot = Bot(token=token)
+    await bot.send_message(m.from_user.id, f'привет {m.from_user.id}')
 
+async def start():
+    # logging.basicConfig(level=logging.INFO) #разкоментировать для отлпдки
+    bot = Bot(token=token, default=DefaultBotProperties(parse_mode='HTML'))
+
+    #Обьявления диспатчера
     dp = Dispatcher()
 
+    #===========DP REGISTERS======================
+    dp.startup.register(start_bot)
+
+    dp.message.register(get_start, Command(commands='start'))
+    #==============================================
+
+
+    #запуск и остановка бота
     try:
         await dp.start_polling(bot)
     finally:
         bot.session.close()
 
-    dp.startup.register(start_bot)
-    dp.message.register()
-
-
-
-
-
-
-
-
-
-
+#Бесконечный цикл для бота
 if __name__ == '__main__':
     asyncio.run(start())
-
